f9e86a62daa5f2c7249ac3ce4b62aa4ee3d19170
Alejandro <alejandro.alvarezp@udea.edu.co>
2025-07-14 00:04:45 -0500

    Creacion de Personajes y Obstaculos en Nivel2

--- RedRibbonGame/main.cpp
+++ RedRibbonGame/main.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QtGlobal>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    srand(12345);
     w.show();
     return a.exec();
 }
